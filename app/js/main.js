0c46d88b0ed2528d285249b6941bdc7026d6529d
shohrux <seltashoh@gmail.com>
2019-09-11 18:25:08 +0500

    adedd all page

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,50 @@
 $(document).ready(function () {
-   $('.main-slider').owlCarousel({
-       items: 1,
-       nav: true,
-       navText: [''
-           // '<span class="main-slider-prev main-slider-arrow-item"><img src="images/prev-arrow.png" alt=""></span>',
-           // '<span class="main-slider-next main-slider-arrow-item"><img src="images/next-arrow.png" alt=""></span>',
-       ],
-       dots: true
+    $('.main-slider').owlCarousel({
+        items: 1,
+        nav: true,
+        dots: true
+    });
 
-   });
+    $('.partner-slider').owlCarousel({
+        items: 5,
+        dots: true,
+        loop:  true,
+        margin: 10,
+        center: true
+    });
+
+
+
+    // menu line
+    findActiveLink($('.header-nav-link li.active'));
+    $('.header-nav-link-item').on('mouseover', function () {
+        var lineWidth = $(this).width() + 'px';
+        var leftPosition = parseInt($(this).css('marginLeft')) + $(this).position().left + 'px';
+        menuLine(lineWidth, leftPosition)
+    });
+    $('.header-nav-link ul').on('mouseleave', function () {
+        var activeMenu = $(this).find('li.active');
+        findActiveLink(activeMenu)
+        // menu line
+    });
+
 
 });
+
+// menu line
+
+function findActiveLink (activeMenu) {
+    var lineWidth = activeMenu.width() ? activeMenu.width() + 'px': 0;
+    var leftPosition = activeMenu.css('marginLeft') ? parseInt(activeMenu.css('marginLeft')) + activeMenu.position().left + 'px' : 0;
+    menuLine(lineWidth, leftPosition)
+}
+
+
+function menuLine(width, position) {
+    var line = $('#line');
+    line.css({
+        width: width,
+        left: position
+    });
+}
+// menu line
