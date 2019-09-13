d167f6cee0f5b93495b9322163ff6cd724d1e1b6
shohrux <seltashoh@gmail.com>
2019-09-13 16:05:20 +0500

    finished

--- app/js/main.js
+++ app/js/main.js
@@ -6,13 +6,26 @@ $(document).ready(function () {
     });
 
     $('.partner-slider').owlCarousel({
-        items: 5,
         dots: true,
-        loop:  true,
         margin: 10,
-        center: true
+        responsive: {
+            0: {
+                items: 2,
+                loop: false,
+                center: false
+            },
+            576: {
+                items: 3,
+                center: true,
+                loop: true
+            },
+            992: {
+                items: 5,
+                center: true,
+                loop: true
+            }
+        }
     });
-
 
 
     // menu line
@@ -28,13 +41,35 @@ $(document).ready(function () {
         // menu line
     });
 
+    // modal
+    $('.open-modal').on('click', function (e) {
+        e.preventDefault();
+        var content = $($(this).data('target') + ' div').clone()[0];
+        swal(content, {
+            button: false,
+            className: 'request-modal'
+        });
+        $('.close-modal').on('click', function () {
+            swal.close()
+        })
+        // modal
+    });
+
+    // burger
+    $('.burger').on('click', function () {
+        $(this).toggleClass('open');
+        $('.main-nav').toggleClass('open');
+        $('body').toggleClass('blocked')
+    })
+    // burger
+
 
 });
 
 // menu line
 
-function findActiveLink (activeMenu) {
-    var lineWidth = activeMenu.width() ? activeMenu.width() + 'px': 0;
+function findActiveLink(activeMenu) {
+    var lineWidth = activeMenu.width() ? activeMenu.width() + 'px' : 0;
     var leftPosition = activeMenu.css('marginLeft') ? parseInt(activeMenu.css('marginLeft')) + activeMenu.position().left + 'px' : 0;
     menuLine(lineWidth, leftPosition)
 }
@@ -47,4 +82,6 @@ function menuLine(width, position) {
         left: position
     });
 }
+
+
 // menu line
